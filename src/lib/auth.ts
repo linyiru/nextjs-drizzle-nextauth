27ee0dbf2d52af1f9b756ecbe891f79fbb88b952
Yi-Ru Lin <deduce@gmail.com>
2023-10-14 20:18:13 -0500

    fix: fix login bug for DrizzleAdapter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,8 @@ import { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { db } from '@/lib/db';
+import { accounts, users } from '@/schema';
+import { and, eq } from "drizzle-orm";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -11,5 +13,28 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
 
-  adapter: DrizzleAdapter(db)
+  adapter: getAdapter(),
 };
+
+function getAdapter() {
+  return {
+    // @ts-expect-error FIXME: when we have a better type for this
+    ...DrizzleAdapter(db),
+    async getUserByAccount(providerAccount: { provider: string; providerAccountId: string; }) {
+      const results = await db
+        .select()
+        .from(accounts)
+        .leftJoin(users, eq(users.id, accounts.userId))
+        .where(
+          and(
+            eq(accounts.provider, providerAccount.provider),
+            eq(accounts.providerAccountId, providerAccount.providerAccountId)
+          )
+        )
+        .get();
+
+      return results?.user ?? null;
+    },
+  };
+}
+
